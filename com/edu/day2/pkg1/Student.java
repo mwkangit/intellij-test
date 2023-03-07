982b00b2f37d3cd89653e8c9383807d9cc620e0c
mwkangit <mwkangit@gmail.com>
2023-03-07 16:11:47 +0900

    v2

---
+++ com/edu/day2/pkg1/Student.java
@@ -0,0 +1,50 @@
+package com.edu.day2.pkg1;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String major;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
---
+++ com/edu/day2/pkg1/StudentTest.java
@@ -0,0 +1,10 @@
+package com.edu.day2.pkg1;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("AMY");
+        Student s3 = new Student("Josh", 20);
+        Student s4 = new Student("John", 22, "컴퓨터공학");
+    }
+}
